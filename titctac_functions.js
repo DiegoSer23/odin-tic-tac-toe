ea2a80cbd00acfcab97da32dbcc5e3b8692ae771
DiegoSer23 <dservin2011@hotmail.com>
2026-05-26 21:33:50 -0600

    Base Js and started adding interface HTML/CSS

--- titctac_functions.js
+++ titctac_functions.js
@@ -1,20 +1,61 @@
-function gameBoard(player, coordinate) {
+function gameBoard() {
     let grid = Array.from({ length: 9}, () => 0);
+    const winnerCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
 
     const setCoordinate = (player, coordinate) => {
-        if (player === 1) {
-            grid[coordinate] = 1;
+        grid[coordinate] = player === 1 ? 1 : 2;
+    }
+
+    const checkWinner = () => {
+        for (const combo of winnerCombos) {
+            const [a, b, c] = combo;
+            if (grid[a] > 0 && grid[a] === grid[b] && grid[a] === grid[c]) {
+                return grid[a];
+            }
         }
-        else {
-            grid[coordinate] = 2;
-        }
+        return 0;
     }
 
     const clearBoard = () => {
         grid = Array.from({ length: 9}, () => 0);
     }
 
-    return { setCoordinate, clearBoard };
+    return { setCoordinate, clearBoard, checkWinner };
 }
 
-function gamePlay()
+function player(name) {
+    let score = 0;
+    let user = name;
+
+    const addWin = () => {
+        score++;
+    }
+
+    return { addWin };
+}
+
+function gameController(player1, player2) {
+    let turn = 1;
+    const playerOne = player1;
+    const playerTwo = player2;
+    const board = gameBoard();
+
+    const playRound = (coordinate) => {
+        board.setCoordinate(turn, coordinate);
+        const winner = board.checkWinner();
+        if (winner === 1) {
+            playerOne.addWin();
+        } else if (winner === 2) {
+            playerTwo.addWin();
+        }
+        turn = turn === 1 ? 2 : 1;
+        return winner;
+    }
+
+    return { playRound };
+}
+
